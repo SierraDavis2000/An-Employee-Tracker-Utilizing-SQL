de7f87bd6b85baad64301a9bde07f6061e618177
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-05-14 22:03:02 -0400

    changes to github directory structure

---
+++ SQL/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXIST employeetracking_db;
+CREATE DATABASE employeetracking_db;
+
+USE employeetracking_db; 
+
+CREATE TABLE department ();
+
+CREATE TABLE role ();
+
+CREATE TABLE employee ();
